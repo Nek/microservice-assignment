fa3184fc2588aed7272f817da4620f28790b000e
Nikita Dudnik <nikdudnik@gmail.com>
2017-06-11 01:39:37 +0200

    Implement some model logic

--- api/models/config.js
+++ api/models/config.js
@@ -1,18 +1,81 @@
 const storage = new Map();
 let counter = 0;
 
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const DB_ADDRESS = 'mongodb://mongo:27017';
+mongoose.connect(DB_ADDRESS);
+
+const Schema = mongoose.Schema;
+
+/*
+
+  {
+  client: String,
+  version: String,
+  key: String,
+  value: String
+  }
+
+  */
+
+/*
+  UPDATE
+  нужен последний етаг
+  device version
+  1. найти конфиг
+  2. взять последний етаг
+  3. вставить/обновить ключ значение с последним етагом
+
+  FIND без етаг
+  найти запись и смерджить все ключ значения
+  FIND с етаг
+  найти запись и 
+  нужны device version etag
+*/
+
+const configRecordSchema = new Schema({
+  client: String,
+  version: String,
+  etag: Number,
+  key: String,
+  value: String
+});
+
+configRecordSchema.plugin(require('mongoose-create-or-update'));
+
+const configEtagSchema = new Schema({
+  client: String,
+  version: String,
+  etag: { type: Number, default: 1 }
+});
+
+const ConfigRecord = mongoose.model('ConfigRecord', configRecordSchema);
+const ConfigEtag = mongoose.model('ConfigEtag', configEtagSchema);
+
+function recordsReducer (res, { key, value }) {
+    // return ({ ...res, key, value });
+}
+
 module.exports = {
   find: ({ client, version }) => {
-    const config = storage.get(client + version);
-    return Promise.resolve(config);
+    return ConfigRecord.find({ client, version })
+      .then(records => {
+        if (records.length === 0) return null;
+        return records[0];
+        // return records.reduce(recordsReducer);
+      });
   },
-  create: ({ client, version, key, value }) => {
-    const id = counter ++;
-    storage.set( client + version , { key, value, id: id.toString() });
-    return Promise.resolve(id.toString());
+  upsert: ({ client, version, key, value }) => {
+    return ConfigRecord.createOrUpdate({ client, version, key },
+                                       { client , version, [key]: value });
+    // const id = counter ++;
+    // storage.set( client + version , { key, value, id: id.toString() });
+    // return Promise.resolve(id.toString());
   },
   reset: () => {
-    storage.clear();
-    counter = 0;
+    ConfigRecord.collection.drop();
+    // storage.clear();
+    // counter = 0;
   }
 };
--- test/api/models/config.js
+++ test/api/models/config.js
@@ -6,26 +6,75 @@ describe('config model', () => {
     model.reset();
   });
   describe('find', () => {
-    it('returns Promise(undefined) for missing configuration', (done) => {
-      const p = model.find({ client: "ios", version: 123 });
-      p.then(v => {
-        should.not.exist(v);
-        done();
+    describe('existing config', () => {
+      it('returns Promise(undefined) for missing configuration', (done) => {
+        model.find({ client: 'ios', version: 123 })
+          .then(record => {
+            should.not.exist(record);
+            done();
+          });
+      });
+    });
+    describe('existing config', () => {
+      it('returns Promise(record)', (done) => {
+        const input = { client: "ios", version: 123, key: 'color', value: 'red' };
+        model.upsert(input)
+          .then(()=> model.find({ client: input.client, version: input.version }))
+          .then(record => {
+            record.should.have.properties({
+              client: 'ios',
+              version: 123,
+              color: 'red'
+            });
+            done();
+          })
+          .catch(() => done());
+      });
+    });
+    describe('updated config', () => {
+      it('returns Promise(record) with updated data', (done) => {
+        const input = { client: "ios", version: 123, key: 'color', value: 'red' };
+        const updateInput = { client: "ios", version: 123, key: 'color', value: 'blue' };
+        model.upsert(input)
+          .then(()=> model.find({ client: input.client, version: input.version }))
+          .then(()=> model.upsert({ client: input.client,
+                                    version: input.version,
+                                    color: 'blue'}))
+          .then(record => {
+            record.should.have.properties(input);
+            done();
+          })
+          .catch(() => done());
       });
     });
   });
-  describe('create', () => {
-    it('returns Promise(configurationId)', (done) => {
-      p = model.create({ client: "ios", version: 123, key: 'color', value: 'red' })
-        .then(v => {
-          v.should.be.String();
+  describe('upsert', () => {
+    it('returns new Promise(record)', (done) => {
+      const input = { client: "ios", version: 123, key: 'color', value: 'red' };
+      model.upsert(input)
+        .then(record => {
+          record.should.have.properties({ client: "ios",
+                                          version: 123,
+                                          color: 'red'
+                                        });
           done();
-        });
+        })
+        .catch(() => done());
     });
-  });
-  describe('create and find', () => {
+    it('returns updated Promise(record) for existing config', (done) => {
+      const input = { client: "ios", version: 123, key: 'color', value: 'red' };
+      model.upsert(input)
+        .then(() => model.upsert( Object.merge(input, { value: 'green' }) ))
+        .then(record => {
+          record.should.have.properties(Object.merge(input, { color: 'green' }));
+          done();
+        })
+        .catch(() => done());
+    });
+});
+  describe.skip('upsert and find', () => {
     it('finds created configuration by client and version', (done) => {
-      const p = model.create({ client: "ios", version: 123, key: 'color', value: 'red' })
+      const p = model.upsert({ client: "ios", version: 123, key: 'color', value: 'red' })
             .then(id => Promise.all([model.find({ client: "ios", version: 123 }), id]))
             .then(v => {
               const [ config, id ] = v;
